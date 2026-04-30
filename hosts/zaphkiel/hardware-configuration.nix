536a6ad67d47a93a316f6e474b651afa6bb0f7e4
kkuroma <contact@kuroma.dev>
2026-05-01 03:54:21 +0700

    added hardware configuration to my main rig

--- hosts/zaphkiel/hardware-configuration.nix
+++ hosts/zaphkiel/hardware-configuration.nix
@@ -1,1 +1,12 @@
-{ }
+{ config, lib, pkgs, modulesPath, ... }:
+{
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "uas" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}

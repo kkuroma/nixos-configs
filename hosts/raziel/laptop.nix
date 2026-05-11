79e9049775179f2ee5418b03227992da131a7cc0
kuroma <konkuad2@gmail.com>
2026-05-11 12:23:59 +0900

    more noctalia ipc rawdog

--- hosts/raziel/laptop.nix
+++ hosts/raziel/laptop.nix
@@ -20,10 +20,7 @@
       RemainAfterExit = true;
       User = "kuroma";
       Environment = "XDG_RUNTIME_DIR=/run/user/1000";
-      ExecStart = "${pkgs.writeShellScript "lock-before-sleep" ''
-        /run/current-system/sw/bin/noctalia-shell ipc --any-display call lockScreen lock
-        sleep 1
-      ''}";
+      ExecStart = "/run/current-system/sw/bin/noctalia-shell ipc --any-display call sessionMenu lockAndSuspend";
     };
   };
 
@@ -31,10 +28,17 @@
     SUBSYSTEM=="power_supply", KERNEL=="ucsi-source-psy-USBC000:00[14]", ACTION=="change", RUN+="${pkgs.writeShellScript "charge-limit" ''
       LEFT_ON=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/ucsi-source-psy-USBC000:001/online)
       RIGHT_ON=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/ucsi-source-psy-USBC000:004/online)
+
       if [ "$LEFT_ON" = "1" ]; then
         ${pkgs.fw-ectool}/bin/ectool fwchargelimit 80
       elif [ "$RIGHT_ON" = "1" ]; then
         ${pkgs.fw-ectool}/bin/ectool fwchargelimit 100
+      fi
+
+      if [ "$LEFT_ON" = "1" ] || [ "$RIGHT_ON" = "1" ]; then
+        ${pkgs.util-linux}/bin/runuser -u kuroma -- env XDG_RUNTIME_DIR=/run/user/1000 /run/current-system/sw/bin/noctalia-shell ipc --any-display call idleInhibitor enable
+      else
+        ${pkgs.util-linux}/bin/runuser -u kuroma -- env XDG_RUNTIME_DIR=/run/user/1000 /run/current-system/sw/bin/noctalia-shell ipc --any-display call idleInhibitor disable
       fi
     ''}"
   '';

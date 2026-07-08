4ddbaa2795054c5d2db6131e9ebe315942ce9cdd
kkuroma <contact@kuroma.dev>
2026-07-09 01:35:05 +0900

    metatron sharship

--- hosts/metatron/home.nix
+++ hosts/metatron/home.nix
@@ -4,11 +4,5 @@
   programs.zsh.shellAliases = {
     matrix-add-user = "sudo register_new_matrix_user -k $(sudo cat /run/secrets/matrix/registration-secret) http://localhost:8448";
   };
-
-  home.file.".config/zsh-prompt.zsh".source = ../../config/zsh-prompt.zsh;
-
-  programs.zsh.initContent = ''
-    export TERM=xterm-256color
-    source ~/.config/zsh-prompt.zsh
-  '';
+  # Prompt comes from home/base/starship.nix (noctalia-off → static fallback palette).
 }

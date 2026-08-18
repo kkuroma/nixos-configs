84f8d8756dcf64159f6def77d1af51c5cbf024d1
kkuroma <contact@kuroma.dev>
2026-08-19 06:20:51 +0900

    lowered snapshot freq

--- parts/modules/snapper.nix
+++ parts/modules/snapper.nix
@@ -1,13 +1,17 @@
 { username, config, lib, ... }:
-# Hourly btrfs snapshots of /home (desktops only)
+# Btrfs snapshots of /home every 3 hours (desktops only)
 lib.mkIf (config.host.profile == "desktop") {
+  services.snapper.snapshotInterval = "0/3:00:00";
   services.snapper.configs.home = {
     ALLOW_USERS = [ username ];
     SUBVOLUME = "/home";
     TIMELINE_CREATE = true;
     TIMELINE_CLEANUP = true;
-    TIMELINE_MIN_AGE = 3600; # 60 minute lifetime before nuke
-    TIMELINE_LIMIT_HOURLY = "6"; # keep the last 6 hours
-    TIMELINE_LIMIT_DAILY = "1"; # keep the last day
+    TIMELINE_MIN_AGE = 3600;
+    TIMELINE_LIMIT_HOURLY = "8";
+    TIMELINE_LIMIT_DAILY = "0";
+    TIMELINE_LIMIT_WEEKLY = "1";
+    TIMELINE_LIMIT_MONTHLY = "0";
+    TIMELINE_LIMIT_YEARLY = "0";
   };
 }

767f2590b64a2a6179686c3b69200eb9f0c2ca29
kuroma <konkuad2@gmail.com>
2026-09-25 12:14:29 +0900

    test signed commits

--- home/base/git.nix
+++ home/base/git.nix
@@ -21,5 +21,10 @@ in
       core.autocrlf = "input";
       credential.helper = lib.mkIf isDesktop "${pkgs.gitFull}/bin/git-credential-libsecret";
     };
+    extraConfig = {
+      gpg.format = "ssh";
+      commit.gpgsign = true;
+      user.signingkey = "~/.ssh/id_ed25519.pub";
+    };
   };
 }
